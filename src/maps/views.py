e101a9926d76b7128ca079f1c09f1d73640ddbd0
Botir <botirziyatov@gmail.com>
2020-12-04 12:05:52 +0500

    searching started

--- src/maps/views.py
+++ src/maps/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
-
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from .models import News
 def index(request):
+    q = request.GET.get('q', '')
+    print(q)
+    vector = SearchVector('document_vector')
+    query = SearchQuery(q)
+    model = News.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')
+
+    for data in model:
+        print(data.title)
     return render(request,'map/index.html',{"list":1})
